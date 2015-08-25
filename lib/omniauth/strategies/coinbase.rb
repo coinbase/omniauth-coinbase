f1cff58803cebf130d1a6ef0620b534cf6f859cb
Alex Ianus <hire@alexianus.com>
2015-08-25 18:24:39 -0400

    Upgrade to API v2

--- lib/omniauth/strategies/coinbase.rb
+++ lib/omniauth/strategies/coinbase.rb
@@ -1,30 +1,29 @@
 require 'omniauth-oauth2'
-require 'coinbase'
+require 'coinbase/wallet'
 
 module OmniAuth
   module Strategies
     class Coinbase < OmniAuth::Strategies::OAuth2
       option :name, 'coinbase'
       option :client_options, {
-              :site => 'https://coinbase.com',
+              :site => 'https://www.coinbase.com',
               :authorize_url => 'https://www.coinbase.com/oauth/authorize',
               :token_url => 'https://www.coinbase.com/oauth/token',
               :proxy => ENV['http_proxy'] ? URI(ENV['http_proxy']) : nil,
               :ssl => {
                 :verify => true,
-                :cert_store => ::Coinbase::Client.whitelisted_cert_store
+                :cert_store => ::Coinbase::Wallet::APIClient.whitelisted_certificates
               }
       }
       option :authorize_options, [:scope, :meta]
 
 
-      uid { raw_info['id'] }
+      uid { raw_info.id }
 
       info do
         {
-          :id => raw_info['id'],
-          :name => raw_info['name'],
-          :email => raw_info['email']
+          :id => raw_info.id,
+          :name => raw_info.name
         }
       end
 
@@ -33,11 +32,11 @@ module OmniAuth
       end
 
       def raw_info
-        @raw_info ||= MultiJson.load(access_token.get('/api/v1/users').body)['users'][0]['user']
+        client = ::Coinbase::Wallet::OAuthClient.new(access_token: access_token.token)
+        @raw_info ||= client.current_user
       rescue ::Errno::ETIMEDOUT
         raise ::Timeout::Error
       end
-
     end
   end
 end
--- omniauth-coinbase.gemspec
+++ omniauth-coinbase.gemspec
@@ -5,9 +5,9 @@ require "omniauth-coinbase/version"
 Gem::Specification.new do |s|
   s.name        = "omniauth-coinbase"
   s.version     = OmniAuth::Coinbase::VERSION
-  s.authors     = ["Miguel Palhas"]
-  s.email       = ["mpalhas@gmail.com"]
-  s.homepage    = "https://github.com/naps62/omniauth-coinbase"
+  s.authors     = ["Miguel Palhas", "Alex Ianus"]
+  s.email       = ["mpalhas@gmail.com", "hire@alexianus.com"]
+  s.homepage    = "https://github.com/coinbase/omniauth-coinbase"
   s.summary     = %q{OmniAuth strategy for Coinbase}
   s.description = %q{OmniAuth strategy for Coinbase}
   s.licenses    = ['MIT']
@@ -21,8 +21,8 @@ Gem::Specification.new do |s|
 
   s.add_dependency 'multi_json', '~> 1.3'
   s.add_runtime_dependency 'omniauth-oauth2', '~> 1.2'
-  s.add_runtime_dependency 'coinbase', '~> 2.0'
-  s.add_development_dependency 'rspec', '~> 2.7'
+  s.add_runtime_dependency 'coinbase', '~> 4.0'
+  s.add_development_dependency 'rspec', '~> 3.0'
   s.add_development_dependency 'rack-test', '~> 0.6'
   s.add_development_dependency 'simplecov', '~> 0.10'
   s.add_development_dependency 'webmock', '~> 1.20'
