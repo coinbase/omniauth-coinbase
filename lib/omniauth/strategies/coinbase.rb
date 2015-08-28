f79ef67ea69462a29f597cb929ad345710ceb96b
Alex Ianus <hire@alexianus.com>
2015-08-28 11:56:47 -0400

    Add simple sandbox support

--- lib/omniauth/strategies/coinbase.rb
+++ lib/omniauth/strategies/coinbase.rb
@@ -4,11 +4,23 @@ require 'coinbase/wallet'
 module OmniAuth
   module Strategies
     class Coinbase < OmniAuth::Strategies::OAuth2
+      SANDBOX_URLS = {
+        :site => 'https://sandbox.coinbase.com',
+        :api => 'https://api.sandbox.coinbase.com',
+        :authorize_url => 'https://sandbox.coinbase.com/oauth/authorize',
+        :token_url => 'https://sandbox.coinbase.com/oauth/token',
+      }
+      PRODUCTION_URLS = {
+        :site => 'https://www.coinbase.com',
+        :api => 'https://api.coinbase.com',
+        :authorize_url => 'https://www.coinbase.com/oauth/authorize',
+        :token_url => 'https://www.coinbase.com/oauth/token',
+      }
+
+      # Options
       option :name, 'coinbase'
+      option :sandbox, false
       option :client_options, {
-              :site => 'https://www.coinbase.com',
-              :authorize_url => 'https://www.coinbase.com/oauth/authorize',
-              :token_url => 'https://www.coinbase.com/oauth/token',
               :proxy => ENV['http_proxy'] ? URI(ENV['http_proxy']) : nil,
               :ssl => {
                 :verify => true,
@@ -16,7 +28,6 @@ module OmniAuth
               }
       }
       option :authorize_options, [:scope, :meta]
-
 
       uid { raw_info.id }
 
@@ -32,10 +43,24 @@ module OmniAuth
       end
 
       def raw_info
-        client = ::Coinbase::Wallet::OAuthClient.new(access_token: access_token.token)
+        client = ::Coinbase::Wallet::OAuthClient.new(access_token: access_token.token, api_url: options.sandbox ? SANDBOX_URLS[:api] : PRODUCTION_URLS[:api])
         @raw_info ||= client.current_user
       rescue ::Errno::ETIMEDOUT
         raise ::Timeout::Error
+      end
+
+      def request_phase
+        load_coinbase_urls
+        super
+      end
+
+      def callback_phase
+        load_coinbase_urls
+        super
+      end
+
+      def load_coinbase_urls
+        options.client_options = (options.sandbox ? SANDBOX_URLS : PRODUCTION_URLS).merge(options.client_options)
       end
     end
   end
