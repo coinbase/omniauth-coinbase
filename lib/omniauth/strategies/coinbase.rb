071794a98f6c44a83bee9b1cd997f30fdebe7661
Alex Ianus <hire@alexianus.com>
2016-03-28 10:34:04 -0400

    Better fix for upstream omniauth-oauth2 issue

--- lib/omniauth/strategies/coinbase.rb
+++ lib/omniauth/strategies/coinbase.rb
@@ -38,6 +38,9 @@ module OmniAuth
         raise ::Timeout::Error
       end
 
+      def callback_url
+        options[:redirect_uri] || (full_host + script_name + callback_path)
+      end
     end
   end
 end
